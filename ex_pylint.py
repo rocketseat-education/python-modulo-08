1f83096c99d642f1b16d19c595344605e863911b
Luis Fellipe Batista Silva <63753269+offellipe@users.noreply.github.com>
2024-04-23 23:31:22 -0300

    Aula 03 - Pylint part.1

---
+++ ex_pylint.py
@@ -0,0 +1,12 @@
+''' Alguma documentacao sobre o arquivo '''
+
+print("Ola Mundo")
+
+
+def minha_funcao():
+    ''' alguma coisa '''
+    print("Estou na minha funcao")
+
+# snake_case -> variáveis e funções
+# camelCase -> Não consta
+# PascalCase -> nomes de classes
