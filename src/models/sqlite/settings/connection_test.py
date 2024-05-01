c8e2befde52b7311fbfedc8fc8476ad9cccbc0d5
Luis Fellipe Batista Silva <63753269+offellipe@users.noreply.github.com>
2024-05-01 00:17:11 -0300

    Aula 09 - Testando conexão com banco de dados

--- src/models/sqlite/settings/connection_test.py
+++ src/models/sqlite/settings/connection_test.py
@@ -1,5 +1,12 @@
-from .connection import db_connection_handler
+from connection import db_connection_handler
+from sqlalchemy.engine import Engine
 
 
 def test_connect_to_db():
-    pass
+    assert db_connection_handler.get_engine() is None
+
+    db_connection_handler.connect_to_db()
+    db_engine = db_connection_handler.get_engine()
+
+    assert db_engine is not None
+    assert isinstance(db_engine, Engine)
