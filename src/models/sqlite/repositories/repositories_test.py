85e9817964972012992ac57fc8a06e8b06d93a3b
Luis Fellipe Batista Silva <63753269+offellipe@users.noreply.github.com>
2024-05-02 10:14:07 -0300

    Aula 13 - Sobre testes de integração

--- src/models/sqlite/repositories/repositories_test.py
+++ src/models/sqlite/repositories/repositories_test.py
@@ -1,9 +1,11 @@
+import pytest
 from src.models.sqlite.settings.connection import db_connection_handler
 from .pets_repository import PetsRepository
 
 db_connection_handler.connect_to_db()
 
 
+@pytest.mark.skip(reason="interacao com o banco")
 def test_list_pets():
     repo = PetsRepository(db_connection_handler)
     response = repo.list_pets()
