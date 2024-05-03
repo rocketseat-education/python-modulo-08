2928cbf6e510bba2ba0c62c9ed93d396878bdaa4
Luis Fellipe Batista Silva <63753269+offellipe@users.noreply.github.com>
2024-05-03 11:18:25 -0300

    Aula 19 - Implementando people repository part.2

--- src/models/sqlite/repositories/people_repository.py
+++ src/models/sqlite/repositories/people_repository.py
@@ -1,4 +1,6 @@
+from sqlalchemy.orm.exc import NoResultFound
 from src.models.sqlite.entities.people import PeopleTable
+from src.models.sqlite.entities.pets import PetsTable
 
 
 class PeopleRepository:
@@ -25,3 +27,23 @@ class PeopleRepository:
             except Exception as exception:
                 database.session.rollback()
                 raise exception
+
+    def get_person(self, person_id: int) -> PeopleTable:
+        with self.__db_connection as database:
+            try:
+                person = (
+                    database.session
+                    .query(PeopleTable)
+                    .join(PetsTable, PetsTable.id == PeopleTable.pet_id)
+                    .filter(PeopleTable.id == person_id)
+                    .with_entities(
+                        PeopleTable.first_name,
+                        PeopleTable.last_name,
+                        PetsTable.name.label("pet_name"),
+                        PetsTable.type.label("pet_type")
+                    )
+                    .one()
+                )
+                return person
+            except NoResultFound:
+                return None
--- src/models/sqlite/repositories/repositories_test.py
+++ src/models/sqlite/repositories/repositories_test.py
@@ -3,7 +3,7 @@ from src.models.sqlite.settings.connection import db_connection_handler
 from .pets_repository import PetsRepository
 from .people_repository import PeopleRepository
 
-db_connection_handler.connect_to_db()
+# db_connection_handler.connect_to_db()
 
 
 @pytest.mark.skip(reason="interacao com o banco")
@@ -19,6 +19,7 @@ def test_delete_pet():
     repo = PetsRepository(db_connection_handler)
     repo.delete_pets(name)
 
+
 @pytest.mark.skip(reason="interacao com o banco")
 def test_insert_person():
     first_name = "test name"
@@ -28,3 +29,13 @@ def test_insert_person():
 
     repo = PeopleRepository(db_connection_handler)
     repo.insert_person(first_name, last_name, age, pet_id)
+
+
+@pytest.mark.skip(reason="interacao com o banco")
+def test_get_person():
+    person_id = 1
+
+    repo = PeopleRepository(db_connection_handler)
+    response = repo.get_person(person_id)
+    print(response)
+    print(response.pet_name)
