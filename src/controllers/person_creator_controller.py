757ef7d3f829c4b98ebebd8ea99905c8b7fb50be
Luis Fellipe Batista Silva <63753269+offellipe@users.noreply.github.com>
2024-05-05 18:12:49 -0300

    Aula 21 - Implementando criação de pessoas

--- src/controllers/person_creator_controller.py
+++ src/controllers/person_creator_controller.py
@@ -1,3 +1,5 @@
+import re
+from typing import Dict
 from src.models.sqlite.interfaces.people_repository import (
     PeopleRepositoryInterface
 )
@@ -6,3 +8,33 @@ from src.models.sqlite.interfaces.people_repository import (
 class PersonCreatorController:
     def __init__(self, people_repository: PeopleRepositoryInterface) -> None:
         self.__people_repository = people_repository
+
+    def create(self, person_info: Dict) -> Dict:
+        first_name = person_info["first_name"]
+        last_name = person_info["last_name"]
+        age = person_info["age"]
+        pet_id = person_info["pet_id"]
+
+        self.__validate_first_and_last_name(first_name, last_name)
+        self.__insert_person_in_db(first_name, last_name, age, pet_id)
+        formated_response = self.__format_response
+        return formated_response
+
+    def __validate_first_and_last_name(self, first_name: str, last_name: str) -> None:
+        # Expressão regular para caracteres que não são letras
+        non_valid_caracteres = re.compile(r'[^a-zA-Z]')
+
+        if non_valid_caracteres.search(first_name) or non_valid_caracteres.search(last_name):
+            raise Exception("Nome inválido")
+
+    def __insert_person_in_db(self, first_name: str, last_name: str, age: int, pet_id: int) -> None:
+        self.__people_repository.insert_person(first_name, last_name, age, pet_id)
+
+    def __format_response(self, person_info: Dict) -> Dict:
+        return {
+            "data": {
+                "type": "Person",
+                "count": 1,
+                "attributes": person_info
+            }
+        }
